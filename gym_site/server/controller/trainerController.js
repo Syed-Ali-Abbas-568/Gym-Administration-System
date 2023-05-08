e63bef5d2a0c8e5044b15ae211091cde4dbf198b
Syed Ali Abbas <l201022@lhr.nu.edu.pk>
2023-05-08 08:54:30 +0500

    Trainer files

---
+++ gym_site/server/controller/trainerController.js
@@ -0,0 +1,75 @@
+import trainer from "../schema/trainerSchema.js"
+
+
+
+export const addTrainer = async (request, response) => {
+
+    const train = request.body
+    const newTrainer = new trainer(train)
+    try {
+        await newTrainer.save()
+        response.status(201).json(newTrainer)
+    }
+    catch (error) {
+        console.log("Error in adding new trainer")
+        response.status(409).json({ message: error.message })
+    }
+
+}
+
+
+export const getTrainer = async (request, response) => {
+
+    try {
+
+        const equip = await trainer.find({ _id: request.params.id })
+        response.status(200).json(equip)
+    }
+    catch (error) {
+        response.status(404).json({ message: error.message })
+    }
+
+}
+
+
+
+export const updateTrainer = async (request, response) => {
+
+    try {
+
+        await trainer.updateOne({ _id: request.params.id }, request.body)
+        response.status(201).json(equip)
+    }
+    catch (error) {
+        response.status(409).json({ message: error.message })
+    }
+
+}
+
+
+export const getTrainerlist = async (request, response) => {
+
+    try {
+
+        const equip = await trainer.find({})
+        response.status(200).json(equip)
+    }
+    catch (error) {
+        response.status(404).json({ message: error.message })
+    }
+
+}
+
+
+export const deleteTrainer=async(request,response)=>{
+
+    try{
+
+ await trainer.deleteOne({_id:request.params.id})
+ response.status(201).json({message:"Deleted Successfully"})
+    }
+    catch(error){
+response.status(409).json({message:error.message})
+    }
+
+}
---
+++ gym_site/src/components/trainerMain.js
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea,Box} from '@mui/material';
+import { Link } from 'react-router-dom';
+
+
+
+export default function TrainerCards() {
+
+
+  return (
+
+<Box sx={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-evenly'}}>
+
+    <Link style={{ textDecoration: 'none' }} to="/addtrainer">
+    <Card sx={{m:5,width:400 ,height:600}}>
+      <CardActionArea>
+      <CardMedia
+          component="img"
+          height="500"
+          image="./addClient.jpg"
+          alt="addequipment"
+          
+         />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          Add Trainer
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+           Click to add Trainer
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    </Link>
+
+
+    <Link style={{ textDecoration: 'none' }} to="/trainerlist">
+    <Card sx={{m:5,width:400 ,height:600}}>
+      <CardActionArea>
+      <CardMedia
+          component="img"
+          height="500"
+          image="./userList.png"
+          alt="gymequipment"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          Display Trainers
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+           Click to view list of Trainers
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+</Link>
+
+
+
+<Link style={{ textDecoration: 'none' }} to="/trainerFeeStatus">
+    <Card sx={{m:5,width:400 ,height:600}}>
+      <CardActionArea>
+      <CardMedia
+          component="img"
+          height="500"
+          image="./feeStatus.png"
+          alt="addequipment"
+          color='white'
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+           View and change fee status
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+           Click to  View and change fee status
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+</Link>
+
+
+</Box>   
+  );
+}
